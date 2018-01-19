378cf3812defa7a8814a3c8f7b3b8112a803243e
Jesse Lopez <Jlopez20515@gmail.com>
2018-01-18 20:39:31 -0800

    pushed to git-hub

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: uluru
+  let losAngeles = {lat: 34.0522, lng: -118.2437};
+  let map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: losAngeles
   });
-  var marker = new google.maps.Marker({
-    position: uluru,
+  
+  let marker = new google.maps.Marker({
+    position: losAngeles,
     map: map
   });
 }
