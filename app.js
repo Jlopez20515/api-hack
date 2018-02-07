80c75398fc0e3ef2f86acdf8ebdc831360ea33d8
Jesse Lopez <Jlopez20515@gmail.com>
2018-02-06 20:10:22 -0800

    added design to css and another object to JS

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-const autocompleteUrl = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=6OZLurgb9tXTUxFourfGrlDeE3pIVPLU&language=en-us&q=';
-// const autocompleteUrl = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=iLVflnmtCAdI8CSd9fnIgEh5Ys2jx2iY&language=en-us&q=';
-
-const getWeatherUrl = 'http://dataservice.accuweather.com/currentconditions/v1/';
-
+const STORE = {
+  tempQueryAPIParameter: '&apikey=iLVflnmtCAdI8CSd9fnIgEh5Ys2jx2iY',
+  tempQueryAPIParameter2: 'apikey=iLVflnmtCAdI8CSd9fnIgEh5Ys2jx2iY',
+  cities: [],
+  currentConditions: {},
+  Precip1hr: {}
+}
+const autocompleteUrl = 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete?language=en-us&q=';
+const getCurrentConditions = 'http://dataservice.accuweather.com/currentconditions/v1/';
 const cityUrl = 'http://dataservice.accuweather.com/locations/v1/cities/geoposition/search';
+// apikey=6OZLurgb9tXTUxFourfGrlDeE3pIVPLU';
+// apikey=iLVflnmtCAdI8CSd9fnIgEh5Ys2jx2iY';
 
-
-
-const temporaryVariable = '?apikey=6OZLurgb9tXTUxFourfGrlDeE3pIVPLU';
-// const temporaryVariable = '?apikey=iLVflnmtCAdI8CSd9fnIgEh5Ys2jx2iY';
-
-// 'http://dataservice.accuweather.com/currentconditions/v1/12345?apikey=6OZLurgb9tXTUxFourfGrlDeE3pIVPLU'
-
-function showCityTemp(data) {
-  let temp = data[0].Temperature.Imperial.Value;
-  let weatherText = data[0].WeatherText;
-  let city = data[0].Type;
-    console.log('Does it work?', city);
-  // let city = data[0].LocalizedName;
-  $('#weather').html('Temperature: ' + temp);
-  $('.forecast').html('Description: ' + weatherText);
-  $('#cityName').html('City: ' + city);
-  // console.log(temp, weatherText);
+function showWeatherInfo() {
+  let temperature = STORE.currentConditions.Temperature.Imperial.Value;
+  let tempUnit = STORE.currentConditions.Temperature.Imperial.Unit;
+  let weatherText = STORE.currentConditions.WeatherText;
+  let Precip1hr = STORE.currentConditions.Precip1hr;
+  let city = STORE.cities[0].LocalizedName;
+  let state = STORE.cities[0].AdministrativeArea.LocalizedName;
+  let country = STORE.cities[0].Country.LocalizedName;
+  $('.weather').html(`Temperature: ${temperature}º ${tempUnit}`);
+  $('.forecast').html(`weatherText ${Precip1hr}`);
+  $('.location').html(`Location: ${city}, ${state}, ${country}`);
 }
 
-function getWeather(cityKey) {
-  let tempUrl = getWeatherUrl + cityKey + temporaryVariable;
-  fetch(tempUrl, {
+function getWeather(highestRankedCityKey) {
+  let currentConditionsUrl = getCurrentConditions
+                           + highestRankedCityKey
+                           + '?'
+                           + STORE.tempQueryAPIParameter2;
+  console.log('Current Condiitons URL: ', currentConditionsUrl);
+  fetch(currentConditionsUrl, {
       method: 'GET',
       headers: {"Accept": "application/json"},
       mode: 'cors'
@@ -38,15 +42,17 @@ function getWeather(cityKey) {
         }
         return response.json();
     }).then((data) => {
-        console.log('From fetch() ', data);
-        showCityTemp(data);
+        console.log('Current Weather Condiitons: ', data);
+        STORE.currentConditions = data[0];
+        showWeatherInfo();
     }).catch(function(err) {
-      console.log('goFetch() Error :-S --> utils.js', err);
+      console.log('getWeather() error', err);
       return err;
     });
 }
 
-function queryWeatherAPI(queryUrl) {
+function getCityInfo(queryUrl) {
+  console.log('City Info URL: ', queryUrl);
   fetch(queryUrl, {
       method: 'GET',
       headers: {"Accept": "application/json"},
@@ -59,17 +65,32 @@ function queryWeatherAPI(queryUrl) {
         }
         return response.json();
     }).then((data) => {
-        let cityKey = data[0].Key;
-        getWeather(cityKey);
+        STORE.cities = data;
+        console.log('Array of Cities: ', STORE.cities);
+        let highestRankedCityKey = STORE.cities[0].Key;
+        // let state = STORE.cities[0].AdministrativeArea.LocalizedName;
+        // let country = STORE.cities[0].Country.LocalizedName;
+        // let city = STORE.cities[0].LocalizedName;
+        getWeather(highestRankedCityKey);
     }).catch(function(err) {
-      console.log('goFetch() Error :-S --> utils.js', err);
+      console.log('goFetch() Error :-S', err);
       return err;
     });
 }
 
 function formUrl(cityInput) {
-  let queryUrl = autocompleteUrl + cityInput;
-  queryWeatherAPI(queryUrl);
+  let queryUrl = autocompleteUrl + cityInput + STORE.tempQueryAPIParameter;
+  getCityInfo(queryUrl);
+}
+
+function initializeApp() {
+  $('.js-submit').on('submit', event => {
+    event.preventDefault();
+    let queryTarget = $(event.currentTarget).find('.cityInput');
+    let cityInput = queryTarget.val();
+    queryTarget.val('');
+    formUrl(cityInput);
+  });
 }
 
 function initMap() {
@@ -91,16 +112,6 @@ function initMap() {
     position: whittier,
     map: map,
     draggable: true
-  });
-}
-
-function initializeApp() {
-  $('.js-submit').on('submit', event => {
-    event.preventDefault();
-    let queryTarget = $(event.currentTarget).find('.cityInput');
-    let cityInput = queryTarget.val();
-    queryTarget.val('');
-    formUrl(cityInput);
   });
 }
 
